69fc1d61b66fe92fbe6a4dc33d2c6e893c510bcb
siriwatknp <siriwatkunaporn@gmail.com>
2025-09-16 11:17:15 +0700

    update ai elements

--- registry/components/ai-image/ai-image.tsx
+++ registry/components/ai-image/ai-image.tsx
@@ -1,12 +1,17 @@
 import { cn } from "@/lib/utils";
 import type { Experimental_GeneratedImage } from "ai";
 
-export type ImageProps = Omit<Experimental_GeneratedImage, "uint8Array"> & {
+export type ImageProps = Experimental_GeneratedImage & {
   className?: string;
   alt?: string;
 };
 
-export const Image = ({ base64, mediaType, ...props }: ImageProps) => (
+export const Image = ({
+  base64,
+  uint8Array,
+  mediaType,
+  ...props
+}: ImageProps) => (
   <img
     {...props}
     alt={props.alt}
